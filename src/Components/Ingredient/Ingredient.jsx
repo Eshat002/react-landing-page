b3872fe7a118514466d8af4d6f957a5765b19ee1
eshat002 <eshatjubayer22@outlook.com>
2023-11-16 15:13:56 +0600

    made responsive

--- src/Components/Ingredient/Ingredient.jsx
+++ src/Components/Ingredient/Ingredient.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Ingredient.css'
 import Ingredient1 from '../../img/ingredient1.1.png'
-import Ingredient2 from '../../img/ingredient2.png'
+import Ingredient2 from '../../img/ingredient2.3.png'
 import Ingredient3 from '../../img/ingredient3.png'
 import Ingredient4 from '../../img/ingredient4.png'
 import WhiteCookies from '../../img/white-cookies.png'
@@ -24,7 +24,7 @@ const Ingredient = () => {
                 <img style={{ width: "100%" }} src={Ingredient1} alt="ingredient1" />
               </div>
               <div>
-                <img style={{ width: "95%", marginTop: "-15px" }} src={Ingredient2} alt="ingredien2" />
+                <img id="ingredient2" style={{ width: "100%" }} src={Ingredient2} alt="ingredien2" />
               </div>
               <div>
                 <img style={{ width: "100%" }} src={Ingredient3} alt="ingredien3" />
